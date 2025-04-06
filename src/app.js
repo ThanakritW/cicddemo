7a90e03bc088beee4d0a1560a4082250093e186c
Thanakrit Wongasa <gainthanakrit@gmail.com>
2025-04-06 22:58:19 +0700

    fix exports

--- src/app.js
+++ src/app.js
@@ -33,6 +33,8 @@ const monthOfTheYear = (date = new Date()) => {
 
 try {
   document.getElementById("day").innerText = dayOfTheWeek();
+  document.getElementById("month").innerText = monthOfTheYear();
 } catch (err) {}
 
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthOfTheYear = monthOfTheYear;
