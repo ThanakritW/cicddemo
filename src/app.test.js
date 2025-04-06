0b3300ffff40a2836fc5dd834d76983c4ed4f2be
Thanakrit Wongasa <gainthanakrit@gmail.com>
2025-04-06 22:37:57 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,10 @@
-const { dayOfTheWeek } = require('./app');
+const { dayOfTheWeek } = require("./app");
 
-
-test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+test("getDay returns the long-format day of the week", () => {
+  const day = dayOfTheWeek(new Date("3/11/2020"));
+  expect(day).toBe("Wednesday");
+});
+test("getDay returns the month of the year", () => {
+  const month = dayOfTheWeek(new Date("3/11/2020"));
+  expect(month).toBe("November");
 });
